42a71b9da1d84f6e7e1eb85334766fc0a5b06930
Russell Ericksen <russbossmoon@gmail.com>
2014-06-13 21:49:35 -0600

    attempting to add more threads running

--- CSThreads/Program.cs
+++ CSThreads/Program.cs
@@ -14,13 +14,17 @@ public class MonitorSample
         int result = 0;   // Result initialized to say there is no error
         Cell cell = new Cell();
 
-        CellProd prod = new CellProd(cell, 20);  // Use cell for storage, 
+        CellProd prod = new CellProd(cell, 50, 2, "Production");  // Use cell for storage, 
         // produce 20 items
-        CellCons cons = new CellCons(cell, 20);  // Use cell for storage, 
+        CellCons cons = new CellCons(cell, 21, 3, "Consumer 1");  // Use cell for storage, 
         // consume 20 items
+        CellCons cons2 = new CellCons(cell, 10, 1, "Consumer 2");  // Use cell for storage, 
+        // consume 20 items
+
 
         Thread producer = new Thread(new ThreadStart(prod.ThreadRun));
         Thread consumer = new Thread(new ThreadStart(cons.ThreadRun));
+        Thread consumer2 = new Thread(new ThreadStart(cons2.ThreadRun));
         // Threads producer and consumer have been created, 
         // but not started at this point.
 
@@ -28,10 +32,12 @@ public class MonitorSample
         {
             producer.Start();
             consumer.Start();
+            consumer2.Start();
 
             producer.Join();   // Join both threads with no timeout
             // Run both until done.
             consumer.Join();
+            consumer2.Join();
             // threads producer and consumer have finished at this point.
         }
         catch (ThreadStateException e)
@@ -57,16 +63,24 @@ public class CellProd
 {
     Cell cell;         // Field to hold cell object to be used
     int quantity = 1;  // Field for how many items to produce in cell
-
-    public CellProd(Cell box, int request)
+    int rate = 1;
+    String name = "";
+    public CellProd(Cell box, int request, int step, String inName)
     {
         cell = box;          // Pass in what cell object to be used
         quantity = request;  // Pass in how many items to produce in cell
+        rate = step; // how many to produce at a time
+        name = inName;
     }
     public void ThreadRun()
     {
+        int val=0;
         for (int looper = 1; looper <= quantity; looper++)
-            cell.WriteToCell(1);  // "producing"
+        {
+            val = cell.WriteToCell(rate, name);  // "producing"
+            
+        }
+
     }
 }
 
@@ -74,18 +88,24 @@ public class CellCons
 {
     Cell cell;         // Field to hold cell object to be used
     int quantity = 1;  // Field for how many items to consume from cell
-
-    public CellCons(Cell box, int request)
+    int rate=1;
+    String name = "";
+    public CellCons(Cell box, int request, int step, String inName)
     {
         cell = box;          // Pass in what cell object to be used
         quantity = request;  // Pass in how many items to consume from cell
+        rate = step;    // how many to remove at a time
+        name = inName;
     }
     public void ThreadRun()
     {
-        int valReturned;
-        for (int looper = 1; looper <= quantity; looper++)
+        int valReturned=0;
+        for (int looper = 1; looper <= quantity; looper++){
             // Consume the result by placing it in valReturned.
-            valReturned = cell.ReadFromCell(2);
+            valReturned = cell.ReadFromCell(rate,name); // "consuming"
+
+        }
+
     }
 }
 
@@ -93,8 +113,10 @@ public class Cell
 {
     int cellContents;         // Cell contents
     bool readerFlag = false;  // State flag
-    public int ReadFromCell(int n)
+
+    public int ReadFromCell(int n, String name)
     {
+        String str = "";
         lock (this)   // Enter synchronization block
         {
             if (!readerFlag)
@@ -113,34 +135,34 @@ public class Cell
                     Console.WriteLine(e);
                 }
             }
+
             if (n > cellContents)
             {
-                Console.WriteLine("Cannot Consume: {0} CellContents: {1}", n, cellContents);
+                str = name +" Cannot Consume: "+n+" CellContents: "+cellContents;
             }
             else
             {
                 cellContents -= n;
                 //Console.WriteLine("Consume: {0}", cellContents);
-                Console.WriteLine("Consume: {0} CellContents: {1}", n, cellContents);
+                str = name +" Consume: "+n+" CellContents: " +cellContents;
             }
-            readerFlag = false;    // Reset the state flag to say consuming
-            // is done.
-            Monitor.Pulse(this);   // Pulse tells Cell.WriteToCell that
-            // Cell.ReadFromCell is done.
+            readerFlag = false;    // Reset the state flag to say consuming is done.
+            Monitor.Pulse(this);   // Pulse tells Cell.WriteToCell that Cell.ReadFromCell is done.
+            Console.WriteLine(str);
         }   // Exit synchronization block
-        return cellContents;
+        return n;
     }
 
-    public void WriteToCell(int n)
+    public int WriteToCell(int n, String name)
     {
+        String str = "";
         lock (this)  // Enter synchronization block
         {
             if (readerFlag)
             {      // Wait until Cell.ReadFromCell is done consuming.
                 try
                 {
-                    Monitor.Wait(this);   // Wait for the Monitor.Pulse in
-                    // ReadFromCell
+                    Monitor.Wait(this);   // Wait for the Monitor.Pulse in ReadFromCell
                 }
                 catch (SynchronizationLockException e)
                 {
@@ -152,11 +174,12 @@ public class Cell
                 }
             }
             cellContents += n;
-            Console.WriteLine("Produce: {0} CellContents: {1}", n, cellContents);
-            readerFlag = true;    // Reset the state flag to say producing
-            // is done
-            Monitor.Pulse(this);  // Pulse tells Cell.ReadFromCell that 
-            // Cell.WriteToCell is done.
+
+            readerFlag = true;    // Reset the state flag to say producing is done
+            Monitor.Pulse(this);  // Pulse tells Cell.ReadFromCell that Cell.WriteToCell is done.
+            str = name +" Produce: " + n + " CellContents: " + cellContents;
+            Console.WriteLine(str); 
+            return n;
         }   // Exit synchronization block
     }
 }
